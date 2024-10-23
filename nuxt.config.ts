611b59288e68ad9b1b30d7a7ce6ed486b6db917d
narr07 <dinarpermadi07@gmail.com>
2024-10-23 18:47:30 +0700

    Add ESLint configuration and integrate with Nuxt; update package.json and VSCode settings

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui', '@nuxt/fonts'],
+  modules: ['@nuxt/ui', '@nuxt/fonts', '@nuxt/eslint'],
   ui: {
 
     global: true,
