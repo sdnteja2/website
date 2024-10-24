165bd0c24b84c1b7f02e1bc2b086394753eb414a
narr07 <dinarpermadi07@gmail.com>
2024-10-25 00:27:41 +0700

    Update styles and components: adjust font size, modify NavBar icons, and add HeroHome component with new layout

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -20,22 +20,31 @@ const links = [
     to: '/artikel',
   }, {
     label: 'Media',
-    icon: 'i-hugeicons-image-01',
+    icon: 'i-hugeicons-video-replay',
     to: '/media',
+  }, {
+    label: 'Kegiatan',
+    icon: 'i-hugeicons-image-01',
+    to: '/kegiatan',
   }],
 ]
 </script>
 
 <template>
-  <div class="w-full fixed top-0 z-50 bg-gray-100/75 dark:bg-gelap-900/75 backdrop-blur">
+  <div class="w-full fixed top-0 z-50 bg-merah-600/90 dark:bg-merah-950/85 backdrop-blur">
     <UContainer class="flex h-14 md:h-16 justify-between items-center">
       <div>LOGO</div>
       <div class="flex items-center space-x-4">
         <div class="flex items-center">
           <UHorizontalNavigation
             :ui="{
-              active: 'text-primary-700 dark:text-primary-300 font-black relative after:bg-primary-500 dark:after:bg-primary-400 after:absolute after:bottom-0 after:left-0 after:h-0.5 after:w-full after:rounded-full after:transform after:scale-x-100 after:origin-left after:transition-transform after:duration-300 after:ease-in-out',
-              inactive: 'text-gray-400 dark:text-gray-500 relative after:bg-transparent after:absolute after:bottom-0 after:left-0 after:h-0.5 after:w-full after:rounded-full after:transform after:scale-x-0 after:origin-left after:transition-transform after:duration-300 after:ease-in-out',
+              active: 'text-gelap dark:text-kuning font-semibold relative after:bg-gelap dark:after:bg-kuning after:absolute after:bottom-0 after:left-0 after:h-0.5 after:w-full after:rounded-full after:transform after:scale-x-100 after:origin-left after:transition-transform after:duration-300 after:ease-in-out',
+              inactive: 'text-gray-50 dark:text-gray-500 relative after:bg-transparent after:absolute after:bottom-0 after:left-0 after:h-0.5 after:w-full after:rounded-full after:transform after:scale-x-0 after:origin-left after:transition-transform after:duration-300 after:ease-in-out',
+              icon: {
+                base: 'flex-shrink-0 w-5 h-5 relative',
+                active: 'text-gelap dark:text-kuning',
+                inactive: 'text-white dark:text-gray-500 group-hover:text-gray-700 dark:group-hover:text-gray-200',
+              },
             }"
             :links="links"
             class="hidden md:block"
---
+++ pages/kegiatan.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    Page: kegiatan
+  </div>
+</template>
+
+<style scoped></style>
