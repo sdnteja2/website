0a3344ed581b2ca8c4de00a2daedca0cbbf4fb3b
narr07 <dinarpermadi07@gmail.com>
2024-10-31 00:29:26 +0700

    Add Google site verification meta tag in app.vue

--- app.vue
+++ app.vue
@@ -1,3 +1,11 @@
+<script setup>
+useHead({
+  meta: [
+    { name: 'google-site-verification', content: 'IWLdaiw_f_MRinNkB6OxOpyUW0-GfaCO6Em02Un0Zns' },
+  ],
+})
+</script>
+
 <template>
   <div>
     <NavBar />
